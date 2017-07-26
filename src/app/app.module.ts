db39eb33484801499ca6bdedd8d6dd77bb37d776
nienkebos <nienkebos@live.com>
2017-07-26 11:49:49 +0200

    Installed firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCGuQWghaSuTO7EPA_oJFhlFFhGyIYLPBw",
+    authDomain: "my-app-15beb.firebaseapp.com",
+    databaseURL: "https://my-app-15beb.firebaseio.com",
+    projectId: "my-app-15beb",
+    storageBucket: "my-app-15beb.appspot.com",
+    messagingSenderId: "167787124773"
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
